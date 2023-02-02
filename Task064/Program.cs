b836e14268b332c13351af6a3986ab7e6a167b09
DureginKostya <kot-502@mail.ru>
2023-02-02 07:20:33 +0500

    Added folder Task064

---
+++ Task064/Program.cs
@@ -0,0 +1,29 @@
+﻿/*Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа 
+в промежутке от N до 1. Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"*/
+Console.Clear();
+int GetDataFromUser(string msg)
+{
+    ColorizeText(msg, ConsoleColor.DarkGreen);
+    int variable = 0;
+    if (int.TryParse(Console.ReadLine()!, out variable) && variable > 0)
+        return variable;
+    return variable = GetDataFromUser("Введите повторно значение N (д.б. целочисленным и больше нуля): ");
+}
+void ColorizeText(string msg, ConsoleColor color)
+{
+    Console.ForegroundColor = color;
+    Console.Write(msg);
+    Console.ResetColor();
+}
+string  GetRowOfNumbers  (int value)   
+{
+    if (value == 1)    
+        return value + "\"";
+    return value + ", " + GetRowOfNumbers(value - 1);
+}
+int valueUser = GetDataFromUser("Введите значение N: ");
+string rowNumbers = GetRowOfNumbers(valueUser);
+ColorizeText($"N = {valueUser} -> \"" + rowNumbers, ConsoleColor.DarkYellow);
+Console.WriteLine();
